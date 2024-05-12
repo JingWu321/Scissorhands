25ed80aa95ff9c6906a148531b6f4ff3b3e1c5da
Jing Wu <wwujing123@gmail.com>
2024-05-12 19:14:53 +1000

    Update train_shs.sh

--- SD/train_shs.sh
+++ SD/train_shs.sh
@@ -1,5 +1,1 @@
-
-python shs.py --class_to_forget '0' --train_method full --sparsity 0.9 --lam 0.5 --memory_num 1 --prune_num 10 --lr 1e-5 --epochs 5 --device '0' --project
-python shs.py --class_to_forget '0' --train_method xattn --sparsity 0.9 --lam 0.1 --memory_num 1 --prune_num 10 --lr 1e-5 --epochs 5 --device '2' --project
-python shs.py --class_to_forget '0' --train_method xattn --sparsity 0.9 --lam 0.5 --memory_num 1 --prune_num 10 --lr 1e-5 --epochs 10 --device '3' --project
-
+python shs.py --class_to_forget '0' --train_method full --sparsity 0.995 --lam 0.1 --memory_num 1 --prune_num 1 --lr 5e-5 --epochs 5 --batch_size 16 --device '1'
